eafa31bf5a362f3dd8052e86f49cbf9376f87a7d
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-02 12:03:47 -0300

    feat: Base do sistema - exercício 5

---
+++ Exercício 5/Exercício 5/Program.cs
@@ -0,0 +1,8 @@
+﻿int totalEleitores, votosBranco, votosNulo, votosValidos;
+Console.WriteLine("Qual é a quantidade de eleitores?");
+totalEleitores = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Qual é a quantidade de votos em branco?");  
+votosBranco = int.Parse(Console.ReadLine()!);   
+Console.WriteLine("Qual é a quantidade de votos nulos?");
+votosNulo = int.Parse(Console.ReadLine()!);
+
