0ff3e6c79529c4036f31f0af761af23978b71c23
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-02 11:38:15 -0300

    Feat: Calcular antecessor - exercício 2

---
+++ Exercício2/Exercício2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Olá! Escreva um valor");
+decimal valor = decimal.Parse(Console.ReadLine()!);
+decimal antecessor = valor - 1;
+Console.WriteLine($"O antecessor do {valor} é = {antecessor}");
