07843d0427875538dd8ad8b639b1d68719331cdf
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-03 11:42:18 -0300

    Feat: Percentual de votos - Exercício 5

--- Exercício 5/Exercício 5/Program.cs
+++ Exercício 5/Exercício 5/Program.cs
@@ -8,14 +8,12 @@ Console.WriteLine("Qual é a quantidade de votos em branco?");
 votosBranco = int.Parse(Console.ReadLine()!);   
 Console.WriteLine("Qual é a quantidade de votos nulos?");
 votosNulo = int.Parse(Console.ReadLine()!);
-
 //Cálculo dos percentuais
 percentualBranco = (votosBranco * 100) / totalEleitores;
 percentualNulo = (votosNulo * 100) / totalEleitores; 
 votosValidos = totalEleitores - (votosBranco + votosNulo);
 percentualValidos = (votosValidos * 100) / totalEleitores;
 Console.WriteLine("\n");
-
 //Mostrar o total de cada tipo de voto e o percentual que ele representa
 Console.WriteLine($"Considerando que o total de eleitores é = {totalEleitores}, temos:\n");
 Console.WriteLine($"Votos em branco = {votosBranco}, sendo {percentualBranco}% da votação total\n");
